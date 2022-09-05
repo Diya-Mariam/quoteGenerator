1e5c4712699ab0e634416783edcac40989959678
Diya Mariam Biju <89064152+Diya-Mariam@users.noreply.github.com>
2022-09-05 16:20:10 +0530

    Bookmark_completed

--- src/Components/Bookmark.js
+++ src/Components/Bookmark.js
@@ -4,53 +4,62 @@ import Navs from "./Navs";
 import Cards from "./Cards";
 
 function Bookmark() {
-
   const [quote, setQuote] = useState([]);
   const [author, setAuthor] = useState([]);
   const [id, setID] = useState([]);
 
   const quoteAPI = () => {
+    const quotes = localStorage.getItem("quotesID");
+    const quotesID = JSON.parse(quotes);
 
-    const quotesID = localStorage.getItem("quotesID")
-
-    Array.prototype.forEach.call(quotesID,(id) => {
-      console.log(id)
+    for (let i = 0; i < quotesID.length; i++) {
 
       axios
-        .get(`https://api.quotable.io/quotes/${id}`)
+        .get(`https://api.quotable.io/quotes/${quotesID[i]}`)
         .then((res) => {
           const data = {
             quote: res.data.content,
             author: res.data.author,
-            id: res.data._id
-          }
-          setQuote((prev) => [...prev, data.quote])
-          setAuthor((prev) => [...prev, data.author])
-          setID((prev) => [...prev, data.id])
-
+            id: res.data._id,
+          };
+          setQuote((prev) => [...prev, data.quote]);
+          setAuthor((prev) => [...prev, data.author]);
+          setID((prev) => [...prev, data.id]);
         })
 
         .catch((err) => {
           console.log(err);
-        })
-    })
-  }
+        });
+    }
+  };
 
   useEffect(() => {
-    quoteAPI()
-  }, [])
+    quoteAPI();
+  }, []);
+
+  const quotes = localStorage.getItem("quotesID");
+  const quotesID = JSON.parse(quotes);
 
   return (
     <div className="body1">
-
-      <Navs /><br /><br />
-      <Cards quote={quote} author= {author} id= {id}/>
+      <Navs />
+      <br />
       <br />
 
+      {quotesID.map((quoteID, i) => (
+        <div>
+          <Cards
+            quote={quote[i]}
+            author={author[i]}
+            id={id[i]}
+          />
+          <br />
+        </div>
+      ))}
+
+      <br />
     </div>
   );
-
 }
-
 
 export default Bookmark;
